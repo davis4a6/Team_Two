45167b21e1db1158686b4928f5365cf994a34dcf
bcaldwell19 <bcaldwell19@hotmail.com>
2023-05-01 21:07:47 -0400

    final project submission

--- Segment 2/join.sql
+++
@@ -1,8 +0,0 @@
--- JOIN on Employees and Mental_Health tables
-
-SELECT emp."ID", emp."COUNTRY_WORKING_IN", men."MENTAL_HEALTH_COVERAGE", men."TECH_COMPANY"
-FROM "MENTAL_HEALTH" AS men
-JOIN "EMPLOYEES" AS emp ON
-emp."ID"=men."ID";
-
-
